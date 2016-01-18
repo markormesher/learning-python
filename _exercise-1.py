87a14ea3f8c701b357d193d48e19fff4bb365646
Mark Ormesher <me@markormesher.co.uk>
2016-01-18 09:20:41 +0000

    Exercises

---
+++ _exercise-1.py
@@ -0,0 +1,11 @@
+# with a list
+
+grades = ['Raymond', 92, 'Cynthia', 83, 'Terrill', 64, 'Jennifer', 75, 'Clayton', 88, 'David', 91, 'Bryan', 100, 'Danny', 99]
+
+print 'grades = ' + str(grades)
+
+print grades[0] + ' ' + str(grades[1])
+print grades[2] + ' ' + str(grades[3])
+print grades[4] + ' ' + str(grades[5])
+print grades[6] + ' ' + str(grades[7])
+print grades[8] + ' ' + str(grades[9])
---
+++ _exercise-2.py
@@ -0,0 +1,17 @@
+# with a dictionary
+
+grades = {'Raymond': 92, 'Cynthia': 83, 'Terrill': 64, 'Jennifer': 75, 'Clayton': 88, 'David': 91, 'Bryan': 100, 'Danny': 99}
+
+print 'grades = ' + str(grades)
+
+print 'Raymond: ' + str(grades['Raymond'])
+print 'Cynthia ' + str(grades['Cynthia'])
+print 'Jennifer ' + str(grades['Jennifer'])
+print 'Terrill ' + str(grades['Terrill'])
+print 'Clayton ' + str(grades['Clayton'])
+print 'David ' + str(grades['David'])
+print 'Bryan ' + str(grades['Bryan'])
+print 'Danny ' + str(grades['Danny'])
+
+print 'names = ' + str(grades.keys())
+print 'scores = ' + str(grades.values())
