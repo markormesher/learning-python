63c9e5c166643b6bc1799d791a798a1e57c33ed8
Mark Ormesher <me@markormesher.co.uk>
2016-01-18 08:51:49 +0000

    Data structures: lists

---
+++ 1-data-types-and-structures/lists.py
@@ -0,0 +1,22 @@
+# empty list
+emptylist = []
+print 'emptylist = ' + str(emptylist) # []
+
+# number list
+numbers = [1, 2, 3, 4]
+print 'numbers = ' + str(numbers) # [1, 2, 3, 4]
+
+# indexing
+print 'numbers[0] = ' + str(numbers[0]) # 1
+
+# slicing
+print 'numbers[1:3] = ' + str(numbers[1:3]) # [1, 2]
+
+# length
+print 'len(numbers) = ' + str(len(numbers)) # 4
+
+# replication
+print 'numbers * 3 = ' + str(numbers * 3) # [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+
+# joining
+print 'numbers + numbers = ' + str(numbers + numbers) # [1, 2, 3, 4, 1, 2, 3, 4]
