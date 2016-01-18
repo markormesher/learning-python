127af4702352c5469533c70212f79dc394fdf238
Mark Ormesher <me@markormesher.co.uk>
2016-01-18 07:32:30 +0000

    Data types: strings

---
+++ 1-data-types/strings.py
@@ -0,0 +1,20 @@
+# concat
+helloworld = 'Hello ' + 'World!'
+print 'helloworld = ' + helloworld
+
+# length
+print 'len(helloworld) = ' + str(len(helloworld))
+
+# indexing
+print 'helloworld[0] = ' + str(helloworld[0])
+
+# slicing
+print 'helloworld[0:4] = ' + str(helloworld[0:4])
+print 'helloworld[:6] = ' + str(helloworld[:6])
+print 'helloworld[3:] = ' + str(helloworld[3:])
+
+# repeating
+print 'helloworld*3 = ' + helloworld*3
+
+# splitting
+print 'helloworld.split(\' \') = ' + str(helloworld.split(' '))
