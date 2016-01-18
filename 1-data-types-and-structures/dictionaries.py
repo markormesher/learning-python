90a080a46ffd7aed48525c3e021e5f35776eeba2
Mark Ormesher <me@markormesher.co.uk>
2016-01-18 09:01:04 +0000

    Data structures: dictionaries

---
+++ 1-data-types-and-structures/dictionaries.py
@@ -0,0 +1,11 @@
+# empty
+emptydictionary = {}
+print 'emptydictionary = ' + str(emptydictionary) # {}
+
+# phones
+phones = {'Mark': '123', 'Ana': '456'}
+print 'phones = ' + str(phones) # {'Ana': '456', 'Mark': '123'}
+
+# keys and values
+print 'phones.keys() = ' + str(phones.keys()) # ['Ana', 'Mark']
+print 'phones.values() = ' + str(phones.values()) # ['456', '123']
