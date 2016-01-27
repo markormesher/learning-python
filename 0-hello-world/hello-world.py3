b801c279ce0b8b05500b51f1a98f8d55526bdf80
Mark Ormesher <me@markormesher.co.uk>
2016-01-27 08:12:50 +0000

    Some more comments

--- 0-hello-world/hello-world.py3
+++ 0-hello-world/hello-world.py3
@@ -1,5 +1,6 @@
+# regular printing
 print("Hello, world")
 
-
+# printing with a different line-end character
 print("Hello, ", end='')
 print("world")
--- 3-decisions/decisions.py
+++ 3-decisions/decisions.py
@@ -1,9 +1,12 @@
 print('What is the answer?')
 ans = int(raw_input())
 
+# if
 if ans < 42:
 	print('Too low')
+# else if
 elif ans == 42:
 	print('Bingo!')
+# else
 else:
 	print('Too high')
