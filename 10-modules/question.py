4ce20fd27a4017816017afa323daa6a2ba0668b0
Mark Ormesher <me@markormesher.co.uk>
2016-01-30 18:51:24 +0000

    Import some functions, not all

--- 10-modules/question.py
+++ 10-modules/question.py
@@ -1,6 +1,8 @@
 # importing our own modules
 from answer import *
 
+# we could also do this, to get just one method:
+# from answer import whatIsTheAnswer
 
 print('What is the answer?')
 print(whatIsTheAnswer())
